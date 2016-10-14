bfe7c484033dc3c3e62e944e7c9b08ad176932ec
Zoer <okamanahi@gmail.com>
2016-10-14 23:18:54 +0300

    First Commit

---
+++ BecomeAPro/BecomeAPro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BecomeAPro.MvcApplication" Language="C#" %>
---
+++ BecomeAPro/BecomeAPro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BecomeAPro.Startup))]
+namespace BecomeAPro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
